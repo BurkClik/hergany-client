d4f83a46b6061c210fc41b862e2bb6f3e315e72a
BurkClik <burk.clik@gmail.com>
2021-03-23 21:49:41 +0300

    i11 için ürünler ekranı düzenlendi

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -7,24 +7,28 @@
             <img :src="require(`@/assets/images/${value.image}`)" alt=""
             class="shadow-xl">
           </div>
-          <div class="col-span-3 px-4 py-4">
-            <p>{{ value.productName }}</p>
-            <p>{{ value.productDetail }}</p>
-            <div class="flex justify-between items-center">
-              <p>{{ value.price }}</p>
-              <button class="bg-fifth text-white">SATIN AL</button>
+          <div class="col-span-3 px-4 py-4 space-y-12">
+            <div class="space-y-3">
+              <p class="josefin text-xl">{{ value.productName }}</p>
+              <p class="roboto-light text-xs">{{ value.productDetail }}</p>
+            </div>
+            <div class="flex justify-between items-center font-light">
+              <p class="roboto-bold text-fifth">{{ value.price }}</p>
+              <button class="roboto bg-fifth text-white px-4 py-1 rounded-2xl text-sm">SATIN AL</button>
             </div>
           </div>
         </div>
       </div>
       <div v-else-if="value.productID % 2 == 0">
         <div class="grid grid-cols-5">
-          <div class="col-span-3 px-4 py-4">
-            <p>{{ value.productName }}</p>
-            <p>{{ value.productDetail }}</p>
+          <div class="col-span-3 px-4 py-4 space-y-12">
+            <div class="space-y-3">
+              <p class="josefin text-xl font-light">{{ value.productName }}</p>
+              <p class="roboto-light text-xs">{{ value.productDetail }}</p>
+            </div>
             <div class="flex justify-between items-center">
-              <p>{{ value.price }}</p>
-              <button class="bg-fifth text-white">SATIN AL</button>
+              <p class="roboto-bold text-fifth">{{ value.price }}</p>
+              <button class="roboto bg-fifth text-white px-4 py-1 rounded-2xl text-sm">SATIN AL</button>
             </div>
           </div>
           <div class="col-span-2 pr-4 py-4">
@@ -49,5 +53,20 @@ export default {
 </script>
 
 <style>
+.josefin {
+  font-family: 'Josefin';
+}
+
+.roboto-light {
+  font-family: 'Roboto-Light';
+}
+
+.roboto-bold {
+  font-family: 'Roboto-Bold';
+}
+
+.roboto {
+  font-family: 'Roboto';
+}
 
 </style>
