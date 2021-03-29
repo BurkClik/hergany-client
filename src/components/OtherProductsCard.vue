f8575f4a112c5845c098d6a06a8f24ed3dd0b37d
BurkClik <burk.clik@gmail.com>
2021-03-29 22:02:27 +0300

    Yeni ürünler sayfası

--- src/components/OtherProductsCard.vue
+++ src/components/OtherProductsCard.vue
@@ -1,16 +1,24 @@
 <template>
-<div class="w-36 text-center">
-    <img class="h-40 w-36" src="../assets/images/ayi.jpg" alt="">
-    <p class="text-sm">Ben bir ceviz ağacıyım gülhane parkında</p>
-</div>
+  <div class="text-center py-6">
+      <img class="h-96 m-auto shadow-md" :src="require(`@/assets/images/${image}`)" alt="">
+      <p class="josefin w-60 text-3xl m-auto pt-2 truncate">{{ productName }}</p>
+      <p class="roboto-bold w-52 text-xl m-auto pt-2">{{ price }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'OtherProductsCard',
+  props: ['productName', 'price', 'image'],
 };
 </script>
 
 <style>
+.josefin {
+  font-family: 'Josefin';
+}
 
+.roboto-bold {
+  font-family: 'Roboto-Bold';
+}
 </style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,60 +1,22 @@
 <template>
-  <div>
-    <div v-for="value in object" class="value pt-4" v-bind:key="value.productID">
-      <div v-if="value.productID % 2 == 1">
-        <div class="flex bg-demo xl:px-64">
-          <div class="flex-1 py-6">
-            <img :src="require(`@/assets/images/${value.image}`)" alt=""
-            class="shadow-xl xl:w-80">
-          </div>
-          <div class="flex-1 px-4 py-6 space-y-12 xl:space-y-52">
-            <div class="space-y-3 xl:space-y-10">
-              <p class="josefin text-xl xl:text-4xl">{{ value.productName }}</p>
-              <p class="roboto-light text-xs xl:text-2xl">{{ value.productDetail }}</p>
-            </div>
-            <div class="flex justify-between items-center font-light">
-              <p class="roboto-bold xl:text-2xl">{{ value.price }}</p>
-              <div class="space-x-8">
-                <span class="text-secondary xl:text-2xl">Trendyol'dan</span>
-                <button class="roboto bg-fifth text-white px-4 py-1 rounded-2xl text-sm xl:px-10 xl:py-1 xl:text-2xl">
-                SATIN AL
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div v-else-if="value.productID % 2 == 0">
-        <div class="flex xl:px-64">
-          <div class="flex-1 px-4 py-6 space-y-12 xl:space-y-52">
-            <div class="space-y-3 xl:space-y-10">
-              <p class="josefin text-xl xl:text-4xl">{{ value.productName }}</p>
-              <p class="roboto-light text-xs xl:text-2xl">{{ value.productDetail }}</p>
-            </div>
-            <div class="flex justify-between items-center">
-              <p class="roboto-bold xl:text-2xl">{{ value.price }}</p>
-              <div class="space-x-8">
-                <span class=" text-secondary xl:text-2xl">Trendyol'dan</span>
-                <button class="roboto bg-fifth text-white px-4 py-1 rounded-2xl text-sm xl:px-10 xl:py-1 xl:text-2xl">
-                  SATIN AL
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="flex-1 py-6">
-            <img :src="require(`@/assets/images/${value.image}`)" alt=""
-            class="shadow-xl xl:w-80 ml-auto">
-          </div>
-        </div>
-      </div>
+  <div class="grid lg:grid-cols-2 xl:grid-cols-3 container mx-auto">
+    <div v-for="product in object" v-bind:key="product.productID">
+      <OtherProductsCard
+        v-bind:productName='`${product.productName}`'
+        v-bind:image='`${product.image}`'
+        v-bind:price='`${product.price}`'/>
     </div>
   </div>
 </template>
 
 <script>
+import OtherProductsCard from '@/components/OtherProductsCard.vue';
 import Data from '../data/productDetails';
 
 export default {
+  components: {
+    OtherProductsCard,
+  },
   data() {
     return {
       object: Data,
