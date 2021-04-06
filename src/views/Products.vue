85c734c7258065bccf184b59a69a421f752f8c11
BurkClik <burk.clik@gmail.com>
2021-04-06 14:51:54 +0300

    Yepis yeni Product Page

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="grid lg:grid-cols-2 xl:grid-cols-3 container mx-auto">
-    <div v-for="product in object" v-bind:key="product.productID">
-      <OtherProductsCard
-        v-bind:productName='`${product.productName}`'
-        v-bind:image='`${product.image}`'
-        v-bind:price='`${product.price}`'/>
+  <div class="grid m-auto container sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+    <div v-for="product in Data" v-bind:key="product.productID">
+      <div class="h-420 w-80 my-6 bg-demo rounded-3xl shadow-xl m-auto sm:mb-12 sm:mt-4 sm:h-80 sm:w-64">
+        <div class="flex flex-col items-center justify-center m-auto h-420 sm:h-80">
+          <img class="h-72 mb-4 shadow-xl sm:h-52 sm:mb-2" :src="require(`@/assets/images/${product.image}`)" alt="">
+          <p class="josefin-semibold text-2xl w-64 line-clamp-1 text-center sm:text-base">{{ product.productName }}</p>
+          <p class="josefin-semibold text-fifth text-xl sm:text-base">{{ product.price }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import OtherProductsCard from '@/components/OtherProductsCard.vue';
+// import OtherProductsCard from '@/components/OtherProductsCard.vue';
 import Data from '../data/productDetails';
 
 export default {
   components: {
-    OtherProductsCard,
+    // OtherProductsCard,
   },
   data() {
     return {
-      object: Data,
+      Data,
     };
   },
 };
@@ -40,6 +43,10 @@ export default {
 
 .roboto {
   font-family: 'Roboto';
+}
+
+.josefin-semibold {
+  font-family: 'Josefin-SemiBold';
 }
 
 </style>
