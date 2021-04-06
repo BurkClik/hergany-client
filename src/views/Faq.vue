378fd363d48f4ef9cf875da94ae776d6aa6d51c3
BurkClik <burk.clik@gmail.com>
2021-04-06 19:18:42 +0300

    Faq Products

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="faq md:grid md:grid-cols-5 md:px-24 lg:px-36 xl:px-48 2xl:px-60">
     <div class="md:col-span-2 md:m-auto">
-      <img class="hidden md:flex
-      md:h-56 md:w-48
-      lg:h-64 lg:w-56
-      xl:h-72 xl:w-64
-      2xl:h-80 2xl:w-72" src="../assets/images/faq_image.png" alt="">
+      <transition appear @before-enter="beforeEnter" @enter="enter">
+        <img class="hidden md:flex
+        md:h-56 md:w-48
+        lg:h-64 lg:w-56
+        xl:h-72 xl:w-64
+        2xl:h-80 2xl:w-72" src="../assets/images/faq_image.png" alt="">
+      </transition>
     </div>
     <div class="md:col-span-3 md:overflow-auto md:mb-10
     xl: h-512
@@ -20,11 +22,29 @@
 </template>
 
 <script>
+import gsap from 'gsap';
 import FaqCard from '@/components/FaqCard.vue';
 
 export default {
   components: {
     FaqCard,
+  },
+  methods: {
+    beforeEnter(el) {
+      gsap.set(el, {
+        opacity: 0,
+        translateY: '-200px',
+      });
+    },
+    enter(el, done) {
+      gsap.to(el, {
+        opacity: 1,
+        y: 0,
+        duration: 2,
+        ease: 'bounce',
+        onComplete: done,
+      });
+    },
   },
 };
 </script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="grid m-auto container sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
     <div v-for="product in Data" v-bind:key="product.productID">
-      <div class="h-420 w-80 my-6 bg-demo rounded-3xl shadow-xl m-auto sm:mb-12 sm:mt-4 sm:h-80 sm:w-64">
-        <div class="flex flex-col items-center justify-center m-auto h-420 sm:h-80">
-          <img class="h-72 mb-4 shadow-xl sm:h-52 sm:mb-2" :src="require(`@/assets/images/${product.image}`)" alt="">
-          <p class="josefin-semibold text-2xl w-64 line-clamp-1 text-center sm:text-base">{{ product.productName }}</p>
-          <p class="josefin-semibold text-fifth text-xl sm:text-base">{{ product.price }}</p>
+      <transition appear @before-enter="beforeEnter" @enter="enter">
+        <div class="h-420 w-80 my-6 bg-demo rounded-3xl shadow-xl m-auto sm:mb-12 sm:mt-4 sm:h-80 sm:w-64">
+          <div class="flex flex-col items-center justify-center m-auto h-420 sm:h-80">
+            <img class="h-72 mb-4 shadow-xl sm:h-52 sm:mb-2" :src="require(`@/assets/images/${product.image}`)" alt="">
+            <p class="josefin-semibold text-2xl w-64 line-clamp-1 text-center sm:text-base">{{ product.productName }}</p>
+            <p class="josefin-semibold text-fifth text-xl sm:text-base">{{ product.price }}</p>
+          </div>
         </div>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-// import OtherProductsCard from '@/components/OtherProductsCard.vue';
+import gsap from 'gsap';
 import Data from '../data/productDetails';
 
 export default {
@@ -24,6 +26,22 @@ export default {
     return {
       Data,
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      gsap.set(el, {
+        opacity: 0,
+        translateX: '-200px',
+      });
+    },
+    enter(el, done) {
+      gsap.to(el, {
+        opacity: 1,
+        x: 0,
+        duration: 2,
+        onComplete: done,
+      });
+    },
   },
 };
 </script>
